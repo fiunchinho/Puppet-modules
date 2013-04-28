428b7cf9ed3d7c14d83bfe6b2a5a92d495c731d6
fiunchinho <github@armesto.net>
2013-04-28 21:33:58 +0000

    Adding packages for PHP

--- apache/manifests/init.pp
+++ apache/manifests/init.pp
@@ -4,27 +4,36 @@ class apache {
 			ensure 	=> present,
 			name	=> 'apache2',
 	}
-	file {
-		'/etc/apache2/apache2.conf':
-			source 	=> 'puppet:///modules/apache/apache2.conf',
-			owner	=> 'root',
-			group	=> 'root',
-			mode	=> '0600',
+
+	exec {
+		'a2enmod rewrite':
+			command => "a2enmod rewrite",
 			require => Package['apache2'],
+			path => ['/usr/bin','/bin', '/usr/sbin'],
+			unless => "/bin/readlink -e /etc/apache2/mods-enabled/rewrite.load"
 	}
+
+	#file {
+	#	'/etc/apache2/apache2.conf':
+	#		source 	=> 'puppet:///modules/apache/apache2.conf',
+	#		owner	=> 'root',
+	#		group	=> 'root',
+	#		mode	=> '0600',
+	#		require => Package['apache2'],
+	#}
 	service {
 		'apache2':
 			ensure	=> running,
 			enable	=> true,
 			hasstatus => true,
 			hasrestart => true,
-			subscribe => File['/etc/apache2/apache2.conf'],
+			#subscribe => File['/etc/apache2/apache2.conf'],
 	}
-	apache::vhost {
-		'diarioyoya':
-			port	=> '8080',
-			url	=> 'diarioyoya.vm',
-			require	=> Package['apache2'],
-			notify	=> Service['apache2'],
-	}
+	#apache::vhost {
+	#	'diarioyoya':
+	#		port	=> '8080',
+	#		url	=> 'diarioyoya.vm',
+	#		require	=> Package['apache2'],
+	#		notify	=> Service['apache2'],
+	#}
 }
--- php/manifests/init.pp
+++ php/manifests/init.pp
@@ -1,32 +1,42 @@
 class php {
-	package {
-		'php5':
-			ensure 	=> present,
-			name	=> 'php5',
-			# require	=> Exec['php5.4 repository'],
-	}
-	package {
-		['php5-mysql', 'php5-xdebug', 'php-apc', 'php-pear', 'php5-cli', 'php5-memcached', 'php5-imagick']:
-			ensure 	=> present,
-			require	=> Package['php5'],
-	}
-	# package {
-	# 	['python-software-properties']:
-	# 		ensure 	=> present,
-	# }
-	file {
-		'/etc/php5/apache2/php.ini':
-			source 	=> 'puppet:///modules/php/php.ini',
-			owner	=> 'root',
-			group	=> 'root',
-			mode	=> '0600',
-			require => Package['php5'],
-	}
-	# exec {
-	# 	'php5.4 repository':
-	# 		path		=> '/usr/bin',
-	# 		logoutput	=> on_failure,
-	# 		command		=> "apt-add-repository ppa:ondrej/php5 && apt-get update",
-	# 		require		=> Package['python-software-properties'],
-	# }
+    package { 'curl': ensure => installed }
+
+    package { 'php5':
+        ensure 	=> present,
+        name	=> 'php5',
+    }
+
+    package { ['php5-mysql', 'php5-xdebug', 'php-apc', 'php-pear', 'php5-cli', 'php5-memcached', 'php5-imagick', 'php5-curl']:
+        ensure 	=> present,
+        require	=> Package['php5'],
+    }
+
+    exec { 'php-mongo':
+        command => "pecl install mongo",
+        path => '/usr/bin:/bin',
+        require => [ Package['php5'], Package['build-essential'] ],
+        notify => Service['apache2'],
+        unless => "test -f /etc/php5/conf.d/mongo.ini"
+    }
+
+    exec { 'composer-install':
+        command => "curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin",
+        path    => '/usr/bin:/bin:/usr/sbin:/sbin',
+        unless  => "test -f /usr/bin/composer.phar",
+        require => Package['curl'],
+    }
+
+    exec { 'pear-discover':
+        path		=> '/usr/bin',
+        command		=> "pear config-set auto_discover 1",
+        unless 		=> "test -f /usr/bin/phpunit",
+        require		=> Package['php-pear'],
+    }
+
+    exec { 'phpunit-install':
+        command => "pear install --alldeps pear.phpunit.de/PHPUnit",
+        path => '/usr/bin:/bin',
+        unless => "test -f /usr/bin/phpunit",
+        require => Exec['pear-discover'],
+    }
 }
